f8b98821e7d9b07bf9b0895d74c2a5e026864171
sjjeong94 <sjjeong94@gmail.com>
2022-06-10 21:05:54 +0900

    convert to onnx model

---
+++ convert.py
@@ -0,0 +1,48 @@
+import torch
+import onnxruntime
+import numpy as np
+
+import models
+
+
+def convert(
+    model_path='./logs/model.pth',
+    onnx_path='./logs/model.onnx',
+):
+
+    net = models.Tower()
+    net.load_state_dict(torch.load(model_path))
+    net = net.eval()
+
+    x = torch.randn(1, 1, 15, 15)
+    with torch.no_grad():
+        y = net(x)
+
+    torch.onnx.export(
+        net,
+        x,
+        onnx_path,
+        export_params=True,
+        opset_version=10,
+        do_constant_folding=True,
+        input_names=['input'],
+        output_names=['output'],
+        #dynamic_axes={'input': {0: 'batch_size'}, 'output': {0: 'batch_size'}},
+    )
+
+    ort_session = onnxruntime.InferenceSession('./logs/model.onnx')
+
+    def to_numpy(tensor):
+        return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+    # compute ONNX Runtime output prediction
+    ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(x)}
+    ort_outs = ort_session.run(None, ort_inputs)
+
+    # compare ONNX Runtime and PyTorch results
+    np.testing.assert_allclose(
+        to_numpy(y), ort_outs[0], rtol=1e-03, atol=1e-05)
+
+
+if __name__ == '__main__':
+    convert()
--- play.py
+++ play.py
@@ -1,13 +1,14 @@
 import torch
 import numpy as np
 import omok
+import onnxruntime
 
 import models
 
 
 def play(agent=2):
     net = models.Tower()
-    net.load_state_dict(torch.load('logs/model_t_100.pth'))
+    net.load_state_dict(torch.load('logs/model_t_augment_10.pth'))
     net = net.eval()
 
     game = omok.OmokGame()
@@ -26,5 +27,25 @@ def play(agent=2):
             result = game.env(action)
 
 
+def play_onnx(agent=2):
+    ort_session = onnxruntime.InferenceSession('./logs/model.onnx')
+
+    game = omok.OmokGame()
+    while game():
+        if game.env.get_player() == agent:
+            state = game.env.get_state()
+            player = game.env.get_player()
+            opponent = player ^ 3
+            board = np.int8(state == player) - np.int8(state == opponent)
+
+            x = board.astype(np.float32)
+            x = np.reshape(x, (1, 1, 15, 15))
+            ort_inputs = {ort_session.get_inputs()[0].name: x}
+            ort_outs = ort_session.run(None, ort_inputs)
+            out = ort_outs[0].squeeze()
+            action = np.argmax(out)
+            result = game.env(action)
+
+
 if __name__ == '__main__':
-    play(agent=2)
+    play_onnx(agent=2)
